d4f5ad3b3df1a52d0a1ce11474ee6213a413c865
Ben Conner <ben.conner40@gmail.com>
2017-01-11 13:03:41 -0600

    added comments

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ function getMovie(){
     //grabbing API JSON
  $.ajax({url:`https://www.omdbapi.com/?t=${movieTitle}=&plot=full&r=json`})
         .done(function(e) {
+            //reset movie search
             $(".movie-body").empty()
+            //if checks for movie error
             if(e.Error){
                 failMovie(e)
+            //else uses vaild movie info   
             }else{
                 loadMovie(e)
             }
@@ -24,21 +27,21 @@ function failMovie(data){
     console.log("no movie")
 }
 
+//Loads movie card with basic info about movie
 function loadMovie(data){
-    console.log(data)
-    console.log(data.imdbRating)
+    //appends card to html
     $(".movie-body").append(`<div movie-card row col-md-4>
                                 <img src="${data.Poster}" alt="${data.Title} movie poster" class="movie-poster">
                                 <div class="title">Title: ${data.Title}</div>
                                 <div class="year">Year: ${data.Year}</div>
                                 <div class="actors">Main Actors: ${data.Actors}</div>
                             </div>`)
-        vailidateRating(data)
+        validateRating(data)
         watchedCheckbox(data)
         $('#movieTitle').val('').focus()
 }
-
-function vailidateRating(data){
+//Valdates if there is a number rating and rounds it
+function validateRating(data){
     if(data.imdbRating ==="N/A"){
         $(".movie-body").append(`<div class="rating-of-5">No ranking found</div>`)
     }else{
@@ -46,7 +49,7 @@ function vailidateRating(data){
     }
 }
 
-
+//Valdates the watch or un-watched checkbox
 function watchedCheckbox(data){
     $(".movie-body").append(`<label>Check box if watched</label><button type="checkbox" class="watched"></button><input class="btn btn-primary" id="save-movie" type="button" value="Add to My Movies">`)
 }
